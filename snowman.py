1a02c35eb4caee66e8cc5a2d8913033314c7a013
Calvin Kuven <calle@MacBook-Air-von-Calvin.local>
2025-08-22 11:22:33 +0200

    Ask the user if they want to play again or exit  after a game end.

--- snowman.py
+++ snowman.py
@@ -5,4 +5,8 @@ from game_logic import play_game
 
 
 if __name__ == "__main__":
-    play_game()
+    while True:
+        play_game()
+        new_game_or_exit = input("Do you want to play again? (y/n): ")
+        if new_game_or_exit == "n":
+            break
