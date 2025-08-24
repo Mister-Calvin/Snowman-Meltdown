45f73348b25a70d9d2e775d10dea2a5f736b443e
Calvin Kuven <calle@MacBook-Air-von-Calvin.local>
2025-08-24 10:18:53 +0200

    Add module-level docstring to game_logic.py and applied code formatting (Cmd + Alt + L)

--- game_logic.py
+++ game_logic.py
@@ -1,13 +1,24 @@
-from ascii_art import STAGES
+"""
+Contains the game logic for the Snowman Meltdown game.
+
+This module includes:
+- selecting a random secret word,
+- displaying the game state,
+- and handling the full game loop with user interaction.
+"""
 import random
+
+from ascii_art import STAGES
 
 
 def get_random_word():
     """Selects a random word from the list."""
     return WORDS[random.randint(0, len(WORDS) - 1)]
 
+
 # List of secret words
 WORDS = ["python", "git", "github", "snowman", "meltdown"]
+
 
 def display_game_state(mistakes, secret_word, guessed_letters):
     """ Display the snowman stage for the current number of mistakes."""
@@ -21,6 +32,7 @@ def display_game_state(mistakes, secret_word, guessed_letters):
             display_word += "_ "
     print("Word: ", display_word)
     print("\n")
+
 
 def play_game():
     """
